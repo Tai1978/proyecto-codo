ab0a3877506d3ff154eea0b58b8dc39a8abf2adb
Tai1978 <selinatai28@yahoo.com>
2024-05-12 16:55:46 -0300

    js boton pagina destinos
    Se corrige html, css y js para boton mostrar en pag destinos

--- proyecto-codo/js/destinos.js
+++ proyecto-codo/js/destinos.js
@@ -1,12 +1,10 @@
-let botones = document.querySelectorAll('.btn-expandir');
-let textosExpandir = document.querySelectorAll('.text-expandir');
-let imagenExpandir = document.querySelectorAll('.imagen-expandir');
+let boton = document.querySelectorAll(".btn-expandir");
+let mostrarComentario = document.querySelectorAll(".text-expandir");
 
-botones.forEach((elemento, clave) => { 
-    elemento.addEventListener('click', () => {
-        textosExpandir[clave].classList.toggle(".abrir_cerrar");
-        imagenExpandir[clave].classList.toggle(".abrir-cerrar");
+
+
+boton.forEach(function (elemento, indice){
+    elemento.addEventListener("click", function(){
+        mostrarComentario[indice].classList.toggle("mostrar_comentario");
     })
-});
-
-
+})
