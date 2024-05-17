a2f23b010dc1344ba7126739aefbc6d57c73a3d1
Tai1978 <selinatai28@yahoo.com>
2024-05-17 20:08:54 -0300

    pag contacto
    Se corrige html y css

--- proyecto-codo/js/contacto.js
+++ proyecto-codo/js/contacto.js
@@ -1,1 +1,31 @@
 
+const form = document.getElementById('user-form');
+const submitButton = document.getElementById ('submit-btn');
+
+let timeout = null;
+
+document.querySelectorAll('.formulario-contacto1').forEach((box) =>{
+   const boxInput = box.querySelector ('input');
+
+   boxInput.addEventListener('keydown', (event)=>{
+   clearTimeout(timeout);
+   timeout= setTimeout(() => {
+    console.log(`Input ${boxInput.name} value:` , boxInput.value);
+
+      validation(box, boxInput)
+   }, 300);
+   })
+})
+
+validation = (box,boxInput)=>{
+    if(boxInput.name == 'nickname'){
+       console.log ('validacion para nombre') ;
+    if(boxInput.value =""){
+        box.classList.remove('form-success');
+        box.classList.add('form-error');
+    }   else{
+        box.classList.remove('form-error');
+        box.classList.add('form-success');
+    }
+    }
+}
